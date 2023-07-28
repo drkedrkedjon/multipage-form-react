bec1c764c28c46ff94310eec7f818b37a83a8b5e
Sasa Savic Perusina <sasa@bubulazy.com>
2023-07-28 13:08:39 +0100

    Working on Menu and Drawer component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,40 @@
-import NavBar from "./NavBar";
+import Drawer from "./Drawer";
+import useToggle from "../utilities/useToggle";
 import { Menu, Umbrella } from "react-feather";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useToggle(false);
+
   return (
     <header>
       <div className="header-container">
         <div className="header-logo">
-          <Umbrella stroke-width={3} color="var(--color-green)" size={30} />
-          <h1>Weather App</h1>
+          <Umbrella strokeWidth={3} color="var(--color-green)" size={30} />
+          <p>Weather App</p>
         </div>
+
         <div className="header-menu">
-          <Menu size={30} />
-          {/* <NavBar /> */}
+          <button className="menu-btn">
+            <Menu size={30} />
+          </button>
+          {isMenuOpen && (
+            <Drawer>
+              <ul>
+                <li>
+                  <a href="">Home</a>
+                </li>
+                <li>
+                  <a href="">Gallery</a>
+                </li>
+                <li>
+                  <a href="">Photographers</a>
+                </li>
+                <li>
+                  <a href="">Submit Work</a>
+                </li>
+              </ul>
+            </Drawer>
+          )}
         </div>
       </div>
     </header>
