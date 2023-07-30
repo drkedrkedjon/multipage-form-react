eb65f5296fc40a086b5cbcb4d213edf80312211c
Sasa Savic Perusina <sasa@bubulazy.com>
2023-07-30 17:24:31 +0100

    Job cards finished

--- src/components/JobOffers.jsx
+++ src/components/JobOffers.jsx
@@ -7,16 +7,18 @@ export default function JobOffers() {
       <div className="job-offers-container">
         {data.map((item) => {
           return (
-            <div className="job-offers-card" key={item.id}>
-              <h2>{item.title}</h2>
-              <p>
-                <span>Description: </span> {item.description}
-              </p>
-              <p>
-                <span>Salary: </span>
-                {item.salary}
-              </p>
-              <button>Apply to this job</button>
+            <div className="job-offers-container-query" key={item.id}>
+              <div className="job-offers-card">
+                <h2>{item.title}</h2>
+                <p>
+                  <span>Description: </span> {item.description}
+                </p>
+                <p>
+                  <span>Salary: </span>
+                  {item.salary}
+                </p>
+                <button>Apply to this job</button>
+              </div>
             </div>
           );
         })}
