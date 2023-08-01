51c19cb52761f546f94599d1b2c58e32413045fc
Sasa Savic Perusina <sasa@bubulazy.com>
2023-08-01 12:44:49 +0100

    Accordeon finished

--- src/components/job-apply-form/AccordionSIngle.jsx
+++ src/components/job-apply-form/AccordionSIngle.jsx
@@ -1,18 +1,23 @@
 /* eslint-disable react/prop-types */
 import * as Accordion from "@radix-ui/react-accordion";
 import { ChevronDown } from "react-feather";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 export default function AccordionSIngle({ header, content }) {
   return (
     <Accordion.Root type="single" collapsible={true}>
-      <Accordion.Item value="item-1">
-        {/* <Accordion.Header> */}
-        <Accordion.Trigger>
-          <ChevronDown />
-          {header}
-        </Accordion.Trigger>
-        {/* </Accordion.Header> */}
-        <Accordion.Content>{content}</Accordion.Content>
+      <Accordion.Item className="accordeon-border" value="item-1">
+        <Accordion.Header asChild>
+          <h4 className="accordeon-h4">
+            <Accordion.Trigger className="accordeon-trigger">
+              <ChevronDown />
+              {header}
+            </Accordion.Trigger>
+          </h4>
+        </Accordion.Header>
+        <Accordion.Content className="accordion-content">
+          <ReactMarkdown>{content}</ReactMarkdown>
+        </Accordion.Content>
       </Accordion.Item>
     </Accordion.Root>
   );
--- src/components/job-apply-form/OpenAccount.jsx
+++ src/components/job-apply-form/OpenAccount.jsx
@@ -1,9 +1,9 @@
 import AccordionSIngle from "./AccordionSIngle";
 
 const acordeonData = {
-  header: "Why you need one more account?",
+  header: "Why do I need another account?",
   content:
-    "We are not going to collect any of your personal data before you make your account. You may ask why? Well, that will make you be able to delete all your personal data after the selection process is done. Be it successful for both sides or not. We do not share that data with anybody aside of our recruiter. He is the only one able to check. So, register and lets se if we can work together and at the end there is a big red button in your account that will delete your data if you wish so.",
+    "We are not going to collect any of your personal data before you make your account. You may ask why?\n\n Well, that will make you be able to delete all your personal data after the selection process is done. Be it successful  or not. We do not share that data with anybody aside of our recruiter. He is the only one able to check.\n\n So, register and lets se if we can work together and at the end there is a big red button in your account that will delete your data if you wish so. Privacy done well.",
 };
 
 export default function OpenAccount() {
